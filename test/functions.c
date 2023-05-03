23280cfecd03bd7eb0311e0e8e589e84f12e9bed
Mostafa-wael <mostafa.w.k000@gmail.com>
2023-05-03 15:01:16 +0300

    fix functions example

--- test/functions.c
+++ test/functions.c
@@ -2,11 +2,11 @@ print("functions");
 a+1;
 x(1, 2);
 print("x");
-def int y (){
+int y (){
     print("y");
     return 1;
 }
-def int x(int a, int b) {
+int x(int a, int b) {
     print("add");
     return a + b;
 }
