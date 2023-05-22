1d257c53962c63a5cb91969745412733ce9fb051
Mostafa-wael <mostafa.w.k000@gmail.com>
2023-05-22 22:30:16 +0300

    add quads test

---
+++ test/quads.c
@@ -0,0 +1,95 @@
+if (a == 10) {
+    a = 5;
+    print("----");
+}
+else if (a == 12) {
+   if (a == 17) {
+            a = 7;
+            print("----");
+        }
+        else {
+            a = 8;
+            print("----");
+        }
+    print("----");
+}
+else if (a == 14) {
+    a = 7;
+    print("----");
+}
+else 
+{
+    if (a == 16) {
+       a = 7;
+        print("----");
+    }
+    else {
+        a = 100;
+        print("----");
+    }
+    a=8;
+    print("----");
+}
+
+
+
+// print("Addition operation");
+// int x;
+// int y = 7;
+// x = 9+y;
+// int z = 9+y+x;
+
+// print("Subtraction operation");
+// x = y-8;
+// x = y-8-z;
+// x = y+8-z;
+// x = -y+8-z;
+// x = -y;
+
+// print("Multiplication operation");
+// x = y-2*z;
+// x = y*2+z;
+// x = y*(2+z);
+
+// print("Division operation");
+// x = y/2.8;
+// x = y/2/8;
+
+// print("Modulus operation");
+// x = y%2;
+
+// print("Logical operation");
+// int x;
+// bool z=false;
+// bool y=true;
+
+// bool z = (2 < 3) || (5 <= x);
+
+// bool z = !(y);
+// bool z = (true && false);
+
+// print("Bitwise operation");
+// int x;
+// int a=5;
+// int c=10;
+
+// x = a | c ;
+// x = a & c ;
+// x = a ^ c ;
+// x = ~a ;
+
+// x = a << c ;
+// x = a >> c ;
+
+// print("Comparison operation");
+
+// int x;
+// int y=5;
+// bool z=false; 
+// bool z = 2 < 3;
+// bool z = 5 <= x;
+// bool z = 2 > y;
+// bool z = 5 >= 7;
+// bool z = y == 7;
+// bool z = x != y;
+exit;
